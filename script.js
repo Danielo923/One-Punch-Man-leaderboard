4cbc25d98889655eecbd855fc965aabbe7e52f8f
Mike B <mikebalvert@icloud.com>
2024-02-12 15:42:41 +0100

    got data working

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 function getHeroes() {
-    fetch('heroes.json')
-    .then(response => response.json())
-    .then(data => data);
-    return data;
-}
-function leaderboard() {
-   const leaderboardElement = document.getElementById('leaderboard');
-    console.log(getHeroes());
-//    leaderboardElement.innerHTML = 
+    fetch('../heroes.json')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Failed to fetch heroes');
+            }
+            return response.json();
+        })
+        .then(data => {
+            localStorage.setItem("data", JSON.stringify(data));
+            return data;
+        })
+        .catch(error => {
+            console.error('Error fetching heroes:', error);
+            throw error;
+        });
 }
 
+function leaderboard() {
+    if (localStorage.getItem("data")) {
+        console.log("aiejvnsibvishbvr")
+        data = JSON.parse(localStorage.getItem("data"));
+    } else {
+        data = JSON.stringify(getHeroes());
+    }
+    // const leaderboardElement = document.getElementById('leaderboard');
+    // leaderboardElement.innerHTML = 
+}
+
+// localStorage.clear()
 leaderboard();
