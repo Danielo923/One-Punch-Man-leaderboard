32942128e3c867da4e5447737dd7707b94f68db9
Mike B <mikebalvert@icloud.com>
2024-02-13 14:43:08 +0100

    added top 3 leaderboard

--- script.js
+++ script.js
@@ -29,9 +29,21 @@ function leaderboard() {
     } else {
         data = getHeroes();
     }
+    const Top3LeaderboardElement = document.getElementById('top3Leaderboard');
     const eliteLeaderboardElement = document.getElementById('eliteLeaderboard');
     const leaderboardElement = document.getElementById('leaderboard');
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 3; i++) {
+        const item = JSON.parse(data)[i];
+        const listItem = document.createElement('tr');
+        listItem.classList.add('elite-hero-card');
+        listItem.innerHTML = `
+            <td><img class="elite-hero-image" src="${item.image}" alt="Foto of ${item.name}"></td>
+            <td class="elite-hero-name">${item.name}</td>
+            <td class="elite-hero-rank">${item.rank}</td>
+        `;
+        Top3LeaderboardElement.appendChild(listItem);
+    }
+    for (let i = 3; i < 10; i++) {
         const item = JSON.parse(data)[i];
         const listItem = document.createElement('tr');
         listItem.classList.add('elite-hero-card');
@@ -59,8 +71,8 @@ function button(buttonId) {
     document.getElementById(`button1`).innerHTML = "1";
     document.getElementById(`button2`).innerHTML = "2";
     document.getElementById(`button3`).innerHTML = "3";
-    const button = document.getElementById(`button${buttonId}`);
-    button.innerHTML = "Selected!";
+    const buttonElement = document.getElementById(`button${buttonId}`);
+    buttonElement.innerHTML = "Selected!";
 }
 
 // localStorage.clear()
