907ed393b3bd98edda6269cd9be4a6273cfc476f
Mike B <mikebalvert@icloud.com>
2024-02-13 11:30:27 +0100

    updated the leaderboard layout from javascript

--- script.js
+++ script.js
@@ -24,17 +24,30 @@ function leaderboard() {
     } else {
         data = getHeroes();
     }
+    const eliteLeaderboardElement = document.getElementById('eliteLeaderboard');
     const leaderboardElement = document.getElementById('leaderboard');
-    JSON.parse(data).forEach(item => {
+    for (let i = 0; i < 10; i++) {
+        const item = JSON.parse(data)[i];
+        const listItem = document.createElement('tr');
+        listItem.classList.add('elite-hero-card');
+        listItem.innerHTML = `
+            <td><img class="elite-hero-image" src="${item.image}" alt="Foto of ${item.name}"></td>
+            <td class="elite-hero-name">${item.name}</td>
+            <td class="elite-hero-rank">${item.rank}</td>
+        `;
+        eliteLeaderboardElement.appendChild(listItem);
+    }
+    for (let i = 10; i < data.length; i++) {
+        const item = JSON.parse(data)[i];
         const listItem = document.createElement('tr');
         listItem.classList.add('hero-card');
         listItem.innerHTML = `
-            <td class="hero-image"><img src="${item.image}" alt="Foto of ${item.name}"></td>
+            <td><img class="elite-hero-image" src="${item.image}" alt="Foto of ${item.name}"></td>
             <td class="hero-name">${item.name}</td>
             <td class="hero-rank">${item.rank}</td>
         `;
         leaderboardElement.appendChild(listItem);
-    });
+    }
 }
 
 // localStorage.clear()
