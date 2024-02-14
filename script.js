b70f740c14905a3db057d8a103e20ed3b5c48cba
Mike B <mikebalvert@icloud.com>
2024-02-14 16:11:12 +0100

    added top3 stars

--- script.js
+++ script.js
@@ -61,10 +61,10 @@ async function leaderboard() {
         listItem.innerHTML = `
             <td><img class="top3-hero-image" src="${item.image}" alt="Foto of ${item.name}"></td>
             <td class="top3-hero-name">${item.rank} | ${item.name}</td>
-            <td class="hero-name"><span class="star-${star}">
+            <td class="hero-name"><span class="star-top3">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
                 <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.56.56 0 0 0-.163-.505L1.71 6.745l4.052-.576a.53.53 0 0 0 .393-.288L8 2.223l1.847 3.658a.53.53 0 0 0 .393.288l4.052.575-2.906 2.77a.56.56 0 0 0-.163.506l.694 3.957-3.686-1.894a.5.5 0 0 0-.461 0z"/>
-                </svg></span>${sortType}: ${item[sortType]}
+                </svg></span> ${item[sortType]}
             </td>
             <td><h2 class="hero-placement">${i + 1}</h2></td>
         `;
@@ -84,7 +84,7 @@ async function leaderboard() {
         <td class="hero-name"><span class="star-${star}">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
             <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.56.56 0 0 0-.163-.505L1.71 6.745l4.052-.576a.53.53 0 0 0 .393-.288L8 2.223l1.847 3.658a.53.53 0 0 0 .393.288l4.052.575-2.906 2.77a.56.56 0 0 0-.163.506l.694 3.957-3.686-1.894a.5.5 0 0 0-.461 0z"/>
-            </svg></span>${sortType}: ${item[sortType]}
+            </svg></span> ${item[sortType]}
         </td>
         <td><h2 class="hero-placement">${i + 1}</h2></td>
         `;
@@ -95,7 +95,6 @@ async function leaderboard() {
         const star = starRating(item.rating)
         const listItem = document.createElement('tr');
         listItem.onclick = function () {
-
             window.location.href = '../hero-profile/profile.html';
         };
         listItem.classList.add('hero-card');
@@ -106,7 +105,7 @@ async function leaderboard() {
             <td class="hero-name"><span class="star-${star}">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
                 <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.56.56 0 0 0-.163-.505L1.71 6.745l4.052-.576a.53.53 0 0 0 .393-.288L8 2.223l1.847 3.658a.53.53 0 0 0 .393.288l4.052.575-2.906 2.77a.56.56 0 0 0-.163.506l.694 3.957-3.686-1.894a.5.5 0 0 0-.461 0z"/>
-                </svg></span>${sortType}: ${item[sortType]}
+                </svg></span> ${item[sortType]}
             </td>
         `;
         leaderboardElement.appendChild(listItem);
